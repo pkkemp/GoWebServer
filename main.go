d50803176ad9db70dc8fba13e55aaca3859c71af
Preston Kemp <preston@prestonkemp.com>
2020-01-24 23:54:25 -0600

    Adding some info to server

--- main.go
+++ main.go
@@ -13,42 +13,43 @@ import (
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	// Write "Hello, world!" to the response body
 	time := time2.Now()
-	text := "Hello world!\n" + time.String() + "\n"
+	text := "Trace:\n" + time.String() + "\n"
 	text += "Remote IP:" + 	r.RemoteAddr + "\n"
-	text+= "Forwarded For:" + r.Header.Get("X-Forwarded-For") + "\n"
-	text+= "Forwarding Protocol: " + r.Header.Get("X-Forwarded-Proto") + "\n"
-	text+= "CDN Loop: " + r.Header.Get("CDN-Loop")
+	text += "Forwarded For:" + r.Header.Get("X-Forwarded-For") + "\n"
+	text += "Forwarding Protocol: " + r.Header.Get("X-Forwarded-Proto") + "\n"
+	text += "Cloudflare Ray:" + r.Header.Get("CF-RAY") + "\n"
+	text+= "CDN Loop: " + r.Header.Get("CDN-Loop") + "\n"
 	io.WriteString(w, text)
 }
 
 func main() {
 	// Set up a /hello resource handler
-
-	http.HandleFunc("/hello", helloHandler)
+	http.HandleFunc("/trace", helloHandler)
 
 	// Create a CA certificate pool and add cert.pem to it
 	caCert, err := ioutil.ReadFile("cert.pem")
 	cloudflare, err := ioutil.ReadFile("cloudflare.pem")
+
 	if err != nil {
 		log.Fatal(err)
 	}
-	caCertPool := x509.NewCertPool()
-	caCertPool.AppendCertsFromPEM(caCert)
-	caCertPool.AppendCertsFromPEM(cloudflare)
+		caCertPool := x509.NewCertPool()
+		caCertPool.AppendCertsFromPEM(caCert)
+		caCertPool.AppendCertsFromPEM(cloudflare)
 
-	// Create the TLS Config with the CA pool and enable Client certificate validation
-	tlsConfig := &tls.Config{
-		ClientCAs: caCertPool,
-		ClientAuth: tls.RequireAndVerifyClientCert,
+		// Create the TLS Config with the CA pool and enable Client certificate validation
+		tlsConfig := &tls.Config{
+			ClientCAs:  caCertPool,
+			ClientAuth: tls.RequireAndVerifyClientCert,
+		}
+		tlsConfig.BuildNameToCertificate()
+
+		// Create a Server instance to listen on port 8443 with the TLS config
+		server := &http.Server{
+			Addr:      ":8443",
+			TLSConfig: tlsConfig,
+		}
+
+		// Listen to HTTPS connections with the server certificate and wait
+		log.Fatal(server.ListenAndServeTLS("cert.pem", "key.pem"))
 	}
-	tlsConfig.BuildNameToCertificate()
-
-	// Create a Server instance to listen on port 8443 with the TLS config
-	server := &http.Server{
-		Addr:      ":8443",
-		TLSConfig: tlsConfig,
-	}
-
-	// Listen to HTTPS connections with the server certificate and wait
-	log.Fatal(server.ListenAndServeTLS("cert.pem", "key.pem"))
-}
